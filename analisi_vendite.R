03be4884cbf778c3190ccd320636d9008ca52aca
Claudio <claudio.tubertini@gmail.com>
2016-05-29 13:05:48 +0200

    aggiunta di analisi trend

--- analisi_vendite.R
+++ analisi_vendite.R
@@ -3,6 +3,7 @@ library(scales)
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(forecast)
 
 df = read.xls ("fatture_20160525.xls", sheet = 1, header = TRUE)
 df2 = separate(data = df, col = Cliente_forn, into = c("cliente", "codice"), sep = "\\-")
@@ -24,3 +25,28 @@ ggplot(data = df_librerie,
        aes(Settimana, Valore_fatturato)) +
   stat_summary(fun.y = sum, geom = "bar")
 
+df_librerie_by_week = group_by(df_librerie, Settimana)
+fatturato_settimanale_librerie = summarise(df_librerie_by_week, fatturato = sum(Valore_fatturato), count = n())
+
+ggplot(fatturato_settimanale_librerie, aes(Settimana, fatturato)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area()
+
+myts <- ts(fatturato_settimanale_librerie$fatturato, frequency=4)
+plot.ts(myts)
+components = decompose(myts)
+plot(components)
+myts_forecast = HoltWinters(myts, beta=FALSE, gamma=FALSE)
+plot(myts_forecast)
+# previsioni per il mese successivo
+myts_forecast4 = forecast.HoltWinters(myts_forecast, h=4)
+plot(myts_forecast4)
+
+# fatturato_settimanale_librerie$ID<-seq.int(nrow(fatturato_settimanale_librerie))
+# trend<-lm(ID ~ fatturato, data=fatturato_settimanale_librerie)
+# summary(trend)
+# mts <- ts(df_librerie$monthly_revenue, 
+#           start=c(2012,1),end=c(2016,6),frequency=12) 
+# fit <- stl(mts, s.window="period")
+# plot(fit)
